db0806644ebb81eb50f7bb9c254dad89f4996876
Cedric Molines <cedric.molines@web-atrio.com>
2018-04-15 13:22:23 +0200

    maj 14-04-2018

--- envolduphenix.sql
+++ envolduphenix.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 10 avr. 2018 à 19:03
+-- Généré le :  Dim 15 avr. 2018 à 11:21
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
@@ -22,50 +22,15 @@ SET time_zone = "+00:00";
 -- Base de données :  `envolduphenix`
 --
 
--- --------------------------------------------------------
-
---
--- Structure de la table `event`
---
-
-DROP TABLE IF EXISTS `event`;
-CREATE TABLE IF NOT EXISTS `event` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `master_id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `start` datetime DEFAULT NULL,
-  `end` datetime DEFAULT NULL,
-  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `game` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `style` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `event_type_id` int(11) DEFAULT NULL,
-  `round_id` int(11) DEFAULT NULL,
-  `slots` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_3BAE0AA713B3DB11` (`master_id`),
-  KEY `IDX_3BAE0AA7401B253C` (`event_type_id`),
-  KEY `IDX_3BAE0AA7A6005CA0` (`round_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 --
 -- Déchargement des données de la table `event`
 --
 
 INSERT INTO `event` (`id`, `master_id`, `name`, `start`, `end`, `description`, `game`, `style`, `event_type_id`, `round_id`, `slots`) VALUES
-(2, 15, 'Table Jdr', NULL, NULL, 'Table de jeu de rôle', 'Warhammer', 'Blablabla', 1, 1, 6);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `event_type`
---
-
-DROP TABLE IF EXISTS `event_type`;
-CREATE TABLE IF NOT EXISTS `event_type` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+(2, 16, 'Table Jdr', NULL, NULL, 'Table de jeu de rôle', 'Warhammer', 'Blablabla', 1, 1, 6),
+(3, 10, 'azeaze', NULL, NULL, 'azeaze', 'azeaz', 'azeaze', 1, 4, 4),
+(4, 16, 'test', NULL, NULL, 'setset', 'estset', 'setest', 1, 1, 5),
+(5, 15, 'dim', NULL, NULL, 'fsdf', 'qsd', 'qsf', 1, 4, 6);
 
 --
 -- Déchargement des données de la table `event_type`
@@ -76,45 +41,12 @@ INSERT INTO `event_type` (`id`, `name`) VALUES
 (2, 'wargame'),
 (3, 'boardgame');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `event_user`
---
-
-DROP TABLE IF EXISTS `event_user`;
-CREATE TABLE IF NOT EXISTS `event_user` (
-  `event_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`event_id`,`user_id`),
-  KEY `IDX_92589AE271F7E88B` (`event_id`),
-  KEY `IDX_92589AE2A76ED395` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 --
 -- Déchargement des données de la table `event_user`
 --
 
 INSERT INTO `event_user` (`event_id`, `user_id`) VALUES
-(2, 10),
-(2, 16);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `menu`
---
-
-DROP TABLE IF EXISTS `menu`;
-CREATE TABLE IF NOT EXISTS `menu` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `weight` int(11) NOT NULL,
-  `active` tinyint(1) NOT NULL,
-  `color` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `pic` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+(2, 10);
 
 --
 -- Déchargement des données de la table `menu`
@@ -125,18 +57,6 @@ INSERT INTO `menu` (`id`, `name`, `weight`, `active`, `color`, `pic`) VALUES
 (2, 'Jeu de rôle', 1, 1, 'bd-orange', 'jdr'),
 (3, 'Wargame', 2, 1, 'bd-orange', 'wg'),
 (4, 'Jeu de société', 3, 1, 'bd-bleu', 'jds');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `migration_versions`
---
-
-DROP TABLE IF EXISTS `migration_versions`;
-CREATE TABLE IF NOT EXISTS `migration_versions` (
-  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `migration_versions`
@@ -152,24 +72,6 @@ INSERT INTO `migration_versions` (`version`) VALUES
 ('20180409202307'),
 ('20180409203050');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `round`
---
-
-DROP TABLE IF EXISTS `round`;
-CREATE TABLE IF NOT EXISTS `round` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `start` datetime NOT NULL,
-  `end` datetime NOT NULL,
-  `event_type_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_C5EEEA34401B253C` (`event_type_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 --
 -- Déchargement des données de la table `round`
 --
@@ -181,30 +83,6 @@ INSERT INTO `round` (`id`, `code`, `name`, `start`, `end`, `event_type_id`) VALU
 (4, 'dimanche-journee', 'Dimanche journée', '2018-05-13 11:00:00', '2018-05-13 16:00:00', 1),
 (5, 'vendredi-soir', 'Vendredi Soir', '2018-05-11 20:00:00', '2018-05-12 04:00:00', 3);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `hash` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `valid` tinyint(1) NOT NULL,
-  `created` datetime NOT NULL,
-  `updated` datetime NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `is_active` tinyint(1) NOT NULL,
-  `roles` json NOT NULL COMMENT '(DC2Type:json_array)',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UNIQ_8D93D649F85E0677` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 --
 -- Déchargement des données de la table `user`
 --
@@ -213,25 +91,6 @@ INSERT INTO `user` (`id`, `password`, `hash`, `valid`, `created`, `updated`, `us
 (10, '$2y$13$QqHg8oe/cSrqy8AUy3NXIOb0hlbLgt1063x/Fs/KbuhlEXGtO2y7O', '$2y$13$hoLLPJUHCpmJnnI1OOvFo.RHudwQVnlsfxiGxY4a8Rwx0MHtISUim', 1, '2018-04-02 22:37:50', '2018-04-02 22:37:50', 'Basch', 'Cedric', 'Molines', 'ced_46000@hotmail.com', 0, '[\"ROLE_ADMIN\"]'),
 (15, '$2y$13$QqHg8oe/cSrqy8AUy3NXIOb0hlbLgt1063x/Fs/KbuhlEXGtO2y7O', '$2y$13$QqHg8oe/cSrqy8AUy3NXIOb0hlbLgt1063x/Fs/KbuhlEdXGtO2y7O', 1, '2018-04-09 00:00:00', '2018-04-09 00:00:00', 'Test', 'test', 'test', 'test@mail.com', 1, '[\"ROLE_USER\"]'),
 (16, '$2y$13$QqHg8oe/cSrqy8AUy3NXIOb0hlbLgt1063x/Fs/KbuhlEXGtO2y7O', '$2y$1s3$QqHg8oe/cSrqy8AUy3NXIOb0hlbLgt1063x/Fs/KbuhlEXGtO2y7O', 1, '2018-04-09 00:00:00', '2018-04-09 00:00:00', 'Dreidr', 'aa', 'aa', 'dreidr@mail.com', 1, '[\"ROLE_USER\"]');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `view`
---
-
-DROP TABLE IF EXISTS `view`;
-CREATE TABLE IF NOT EXISTS `view` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `menu_id` int(11) NOT NULL,
-  `link` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `content` longtext COLLATE utf8mb4_unicode_ci,
-  `weight` int(11) NOT NULL,
-  `active` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_FEFDAB8ECCD7E912` (`menu_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `view`
@@ -251,37 +110,6 @@ INSERT INTO `view` (`id`, `name`, `menu_id`, `link`, `content`, `weight`, `activ
 (11, 'Fury : Outburst Control', 3, NULL, '<!-- Texte -->\r\n	<div class=\"row\">\r\n		<div class=\"col-sm-6 col-sm-push-3 col-md\">\r\n			<h3 class=\"titre-page\">Fury : Outburst Control</h3>\r\n			<hr class=\"hr-titre\">\r\n			<h5 class=\"sous-titre\">INITIATION</h5>\r\n		</div> <!-- col -->\r\n	</div> <!-- row -->\r\n	<div class=\"row\">\r\n		<div class=\"col-md-offset-2 col-md-8\">\r\n			<br>\r\n			<p><strong>Fury : Outburst Control</strong> est un jeu d’escarmouche créé et sculpté par Forge Studio. Nous accueillerons Jerôme \"Whispe\" Labadie, son concepteur, pour des parties d\'initiation / de découverte.</p>\r\n			<br>\r\n			<img src=\"{{ asset(\'images/wg/furybanniere1.png\')}}\" class=\"img-responsive img-fury\">\r\n			<br>\r\n			<div class=\"justi-txt\"><p><strong>Fury : Outburst Control*</strong> est un jeu d’escarmouche à activation alternée qui vous offre la possibilité de combattre dans cet univers d’exception.</p>\r\n				<p>Manipulant les énergies de la colère, prenez la tête de patrouilles de 3 à 10 figurines pour des parties ultra-rapides en 125 points ou de groupes bien plus nombreux dans des formats allant jusqu’à 300 points de Patrouille. En utilisant à bon escient le potentiel de vos officiers, accumulez des points de Furie afin de leur faire relâcher des techniques de combat dévastatrices. Apprenez à gérer les différentes postures de combat adaptées à chaque situation afin d’atteindre au mieux vos objectifs.</p></div>\r\n			<br>\r\n			<img src=\"{{ asset(\'images/wg/fury2.jpg\')}}\" class=\"center-block img-rounded img-responsive\">\r\n			<br>\r\n			<p><em>*Source article et photos: <a href=\"www.forge-studio.com/fury\" target=\"_blank\">www.forge-studio.com/fury</a></em></p>\r\n		</div>\r\n	</div>', 4, 1),
 (12, 'Présentation', 4, NULL, '<!-- Texte -->\r\n	<div class=\"row\">\r\n		<div class=\"col-sm-6 col-sm-push-3\">\r\n			<h3 class=\"titre-page\">Présentation</h3>\r\n			<hr class=\"hr-titre\">\r\n			<h5 class=\"sous-titre\">GRATUIT / TOUT PUBLIC</h5>\r\n		</div> <!-- col -->\r\n	</div> <!-- row -->\r\n	<div class=\"row\">\r\n		<div class=\"col-sm-10 col-sm-push-1 col-md-8 col-md-push-2\">\r\n			<br>\r\n			<p>Bang! ... Bang! ... Bang!...</p>\r\n			<div class=\"justi-txt\"><p>Les portes s’ouvrent sur l\'espace Valentré. La pleine lune illumine le ciel pour le bonheur des petits et des grands.  Les sorcières préparent leurs potions et les magiciens leurs formules. Des pactes sont conclus…\r\n			</p>\r\n				<p>À chaque coup sur l\'espace jeux de société du festival l\'Envol du Phénix, vous êtes l\'acteur de scènes épiques.\r\n				</p>\r\n				<p>Les thématiques des jeux de société ont bien évolués ces dernières années. Et les animateurs du Phénix vont vous proposer de :<br>\r\n					Piloter vos figurines de vaisseau spatial avec <strong>X-wing</strong>,<br>\r\n					Chasser du zombie en groupe avec <strong>Zombicide</strong>,<br>\r\n					Vengez un fantôme avec vos talents de médium avec <strong>Mystérium</strong>,<br>\r\n					Retrouvez un coupable parmi six suspects avec les indices de votre <strong>Profiler</strong>,<br>\r\n					... Affrontez les ténèbres et forgez votre propre légende !<br>\r\n				</p>\r\n				<p>Les hostilités débuteront le&nbsp; <strong>Samedi 12 mai de 10h - 20h</strong> gratis et tout public!<br>\r\n					Prolongations le <strong>Dimanche 13 mai de 10h - 18h</strong> gratis et tout public!</p>\r\n			</div>\r\n		</div> <!-- col -->\r\n		<div class=\"col-xs-6 col-sm-4 col-sm-offset-2 col-md-2 col-md-offset-0 col-md-pull-8\">\r\n			<img class=\"img-responsive img-rounded\" src=\"{{ asset(\'images/jds/take.jpg\')}}\">\r\n			<br>\r\n			<img class=\"img-responsive img-rounded\" src=\"{{ asset(\'images/jds/abyss.jpg\')}}\">\r\n			<br>\r\n			<img class=\"img-responsive img-rounded\" src=\"{{ asset(\'images/jds/potion.jpg\')}}\">\r\n		</div>\r\n		<div class=\"col-xs-6 col-sm-4 col-md-2\">\r\n\r\n			<img class=\"img-responsive img-rounded\" src=\"{{ asset(\'images/jds/tablejds.jpg\')}}\">\r\n			<br>\r\n			<img class=\"img-responsive img-rounded\" src=\"{{ asset(\'images/jds/myst.jpg\')}}\">\r\n			<br>\r\n			<img class=\"img-responsive img-rounded\" src=\"{{ asset(\'images/jds/magic.jpg\')}}\">\r\n		</div>\r\n	</div> <!-- row -->', 0, 1),
 (13, 'Soirée Loups-Garous de Thiercelieux', 4, NULL, '<!-- Texte -->\r\n	<div class=\"row\">\r\n		<div class=\"col-sm-6 col-sm-push-3\">\r\n			<h3 class=\"titre-page\">Soirée des Loups-Garous de Thiercelieux</h3>\r\n			<hr class=\"hr-titre\">\r\n			<h5 class=\"sous-titre\">VENDREDI</h5>\r\n		</div> <!-- col -->\r\n	</div> <!-- row -->\r\n	<div class=\"row\">\r\n		<br>\r\n		<img class=\"img-responsive\" src=\"{{ asset(\'images/jds/bobmical.jpg\')}}\">\r\n		<br>\r\n	</div> <!-- row -->\r\n	<div class=\"row\">\r\n		<div class=\"col-md-offset-3 col-md-6\">\r\n			<p>Au village de Thiercelieux, les villageois vivent paisiblement le jour durant.<br>\r\n				Mais lorsque la nuit tombe, certain se transforme en loup garou et attaque discrètement.<br>\r\n				Et au levé du jour, la peur, la méfiance et le silence règnent. Tout ceci doit stopper! Les survivants vont s\'illustrer dans l\'art oratoire pour bannir les loups garous de village!</p>\r\n			<p>Grand conseil le <strong>Vendredi 11 mai à partir de 21h, tout public, pour 2€</strong>!</p>\r\n			<br>\r\n			<img class=\"img-responsive img-rounded\" src=\"{{ asset(\'images/jds/lg2.JPG\')}}\">\r\n			<br>\r\n		</div> <!-- col -->\r\n\r\n	</div> <!-- row -->', 1, 1);
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `event`
---
-ALTER TABLE `event`
-  ADD CONSTRAINT `FK_3BAE0AA713B3DB11` FOREIGN KEY (`master_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `FK_3BAE0AA7401B253C` FOREIGN KEY (`event_type_id`) REFERENCES `event_type` (`id`),
-  ADD CONSTRAINT `FK_3BAE0AA7A6005CA0` FOREIGN KEY (`round_id`) REFERENCES `round` (`id`);
-
---
--- Contraintes pour la table `event_user`
---
-ALTER TABLE `event_user`
-  ADD CONSTRAINT `FK_92589AE271F7E88B` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_92589AE2A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `round`
---
-ALTER TABLE `round`
-  ADD CONSTRAINT `FK_C5EEEA34401B253C` FOREIGN KEY (`event_type_id`) REFERENCES `event_type` (`id`);
-
---
--- Contraintes pour la table `view`
---
-ALTER TABLE `view`
-  ADD CONSTRAINT `FK_FEFDAB8ECCD7E912` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
