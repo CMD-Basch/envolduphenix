fc7073ebc5844755c31a4004e20268485ac758c1
Cedric Molines <cedric.molines@web-atrio.com>
2018-10-19 09:10:00 +0200

    update .gitingnore

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Hillrange\CKEditor\HillrangeCKEditorBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
