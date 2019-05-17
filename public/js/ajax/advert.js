8eb205ce46fe378871d9fc37dbc1ffc1c99365ea
Cédric Molines <cedric.molines@web-atrio.com>
2019-05-17 18:28:32 +0200

    MAj advert

---
+++ public/js/ajax/advert.js
@@ -0,0 +1,61 @@
+$(document).ready( function() {
+
+    const $carousel = $('#carousel');
+
+    $carousel.slick({
+        adaptiveHeight: true,
+        autoplay: true,
+        cssEase: 'linear',
+        speed: 20000,
+        autoplaySpeed: 0,
+        arrows: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        slidesToShow: 3
+    });
+
+    const   $global = $('#global'),
+            round_id = $global.data('round-id')
+    ;
+
+    console.log($global, round_id);
+
+    function refreshRounds() {
+
+
+        $.ajax({
+            url: '/advert/ajax/'+round_id,
+            type: "POST",
+            data:{},
+            async: true,
+            success: function (data) {
+                console.log(data);
+
+                if( data.status !== 'ok') return;
+
+                for( let key in data.blocks ) {
+                    let block =  data.blocks[ key ];
+                    $find = $( '#activity-' + block.id );
+                    if( $find.length > 0 ) {
+                        $find.html( block.html );
+                    } else {
+                        $carousel.slick('slickAdd',
+                            '<div id="activity-' + block.id + '" class="ml-3">'
+                            +   block.html
+                            +   '</div>'
+                        );
+                    }
+                }
+
+
+            }
+        }).always( function () {
+
+        });
+    }
+
+    $("#btn-refresh").on('click', function () {
+        refreshRounds();
+    });
+
+});
--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -7,17 +7,21 @@ use App\Entity\Round;
 use App\Service\Event\EventService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class AdvertController extends AbstractController
 {
 
     private $em;
+    private $twig;
     private $sEvent;
 
-    public function __construct ( EntityManagerInterface $em, EventService $sEvent )
+    public function __construct ( EntityManagerInterface $em, EventService $sEvent, Environment $twig )
     {
         $this->em = $em;
+        $this->twig = $twig;
         $this->sEvent = $sEvent;
     }
 
@@ -39,15 +43,37 @@ class AdvertController extends AbstractController
      */
     public function advertRound( Round $round ) {
 
-        dump( $round->getActivities()->toArray() );
-
-
-//        $type = $this->em->getRepository( ActivityType::class )->findOneBy(['slug' => 'jeu-de-roles']);
-//        $rounds = $this->em->getRepository( Round::class )->findBy(['activityType' => $type]);
-
         return $this->render( 'envol/advert/round.html.twig', [
             'round' => $round,
         ]);
     }
+
+    /**
+     * @Route("/advert/ajax/{round}", name="advert.ajax.round")
+     */
+    public function advertAjaxRound( Round $round ) {
+
+        $activities = $round->getActivities();
+        $blocks = [];
+        foreach ( $activities as $index => $activity ){
+            $block = $this->twig->render('envol/pages/activity/roleplay/block/activity.block.html.twig', [
+                'activity' => $activity,
+                'buttons' => false,
+                'index' => $index,
+            ]);
+
+            $blocks[] = [
+                'html' => $block,
+                'id' => $activity->getId(),
+            ];
+        }
+
+        return new JsonResponse([
+            'status' => 'ok',
+            'blocks' => $blocks
+        ]);
+    }
+
+
 
 }
